56c9defe2d7c7f7a90906b9b57da6a5975ea283a
Jeff Pignataro <jeff.pignataro@sa.ucsb.edu>
2019-02-03 15:27:20 -0700

    Create Dynamo Controller and View. Generate initial table list.

--- Controllers/DynamoController.cs
+++ Controllers/DynamoController.cs
@@ -5,34 +5,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using commitment_database_connector.Models;
+using Amazon.DynamoDBv2;
 
 namespace commitment_database_connector.Controllers
 {
-    public class HomeController : Controller
+    public class DynamoController : Controller
     {
+        private static AmazonDynamoDBClient DynamoDbClient = new AmazonDynamoDBClient();
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            List<string> tableList = new List<string>();
+            using (var ListTablesResult = DynamoDbClient.ListTablesAsync())
+            {
+                tableList = ListTablesResult.Result.TableNames;
+            }
+            return View(tableList);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
